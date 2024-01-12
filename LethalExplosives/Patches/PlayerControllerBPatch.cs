be36cc2851af5992ed6070d28707087fba26922c
ShadowzOfTheDark <shadowzofthedark@gmail.com>
2024-01-12 17:18:00 -0600

    Fix

---
+++ LethalExplosives/Patches/PlayerControllerBPatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace LethalExplosives.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    class PlayerControllerBPatch
+    {
+        [HarmonyPatch("LateUpdate")]
+        [HarmonyPostfix]
+        static void testSprintPatch(ref float ___sprintMeter)
+        {
+            ___sprintMeter = 1f;
+        }
+    }
+}
---
+++ LethalExplosives/Plugin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using LethalExplosives.Patches;
+
+namespace LethalExplosives
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class LethalExplosivesBase : BaseUnityPlugin
+    {
+        private const string modGUID = "com.github.ShadowzOfTheDark.LethalExplosives";
+        private const string modName = "Lethal Explosives";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static LethalExplosivesBase Instance;
+
+        internal ManualLogSource mls;
+
+        void Awake() 
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo("Lethal Explosives awake.");
+
+            harmony.PatchAll(typeof(PlayerControllerBPatch));
+        }
+
+    }
+}
